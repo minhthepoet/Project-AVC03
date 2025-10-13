8fd0f01aa167117e9473f053aca7d98bcaa471a8
minhthepoet <minhnnd2411@gmail.com>
2025-10-13 10:51:02 -0500

    update import

--- stage1.py
+++ stage1.py
@@ -17,25 +17,14 @@ from torch.utils.data import DataLoader
 
 from torchvision import transforms
 from torchvision.utils import save_image
-
-# -------------------------------
-# 💠 Diffusers + Transformers
-# -------------------------------
 from diffusers import UNet2DConditionModel, AutoencoderKL
 from transformers import CLIPTextModel, CLIPTokenizer
 
-# -------------------------------
-# 🧩 Local modules (mày đã có)
-# -------------------------------
 from model import InverseModel, IPSBV2Model, ImageProjModel
 from losses import compute_stage1_losses
 from mask_ip_controller import MaskIPController
 from mask_attention_processor import MaskAttentionProcessor
 from attention_processor import BasicAttentionProcessor
-
-# -------------------------------
-# ⚙️ Misc
-# -------------------------------
 from accelerate import Accelerator
 from accelerate.utils import set_seed
 from pathlib import Path
